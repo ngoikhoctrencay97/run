7759128c4ae61e5a0cebf6b18db45f6174d62194
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2025-07-22 21:30:31 +0700

    Update tmux_monitor.sh

--- tmux_monitor.sh
+++ tmux_monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo -i <<'EOF'
-cat > /root/tmux_monitor.sh << 'SCRIPT'
+cat > /home/ubuntu/tmux_monitor.sh << 'SCRIPT'
 #!/bin/bash
 SESSION_NAME="0"
 WORK_DIR="rl-swarm"
@@ -10,15 +10,19 @@ RUN_SCRIPT="./run_rl_swarm.sh"
 
 if ! /usr/bin/tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
     cd /home/ubuntu
+    sleep 1
     /usr/bin/tmux new-session -d -s "$SESSION_NAME"
+    sleep 1
     /usr/bin/tmux send-keys -t "$SESSION_NAME" "cd $WORK_DIR" Enter
+    sleep 1
     /usr/bin/tmux send-keys -t "$SESSION_NAME" "source $VENV_PATH" Enter
+    sleep 1
     /usr/bin/tmux send-keys -t "$SESSION_NAME" "$RUN_SCRIPT" Enter
 fi
 SCRIPT
 
-chmod +x /root/tmux_monitor.sh
-echo '*/5 * * * * root /root/tmux_monitor.sh' >> /etc/crontab
+chmod +x /home/ubuntu/tmux_monitor.sh
+echo '*/5 * * * * root /home/ubuntu/tmux_monitor.sh' >> /etc/crontab
 systemctl restart cron
 echo "Setup complete"
 EOF
