3aa7e9459ccc74e0cb689f0595c0e1148ecded91
Hoang Nguyen <31663074+ngoikhoctrencay97@users.noreply.github.com>
2025-07-22 23:43:22 +0700

    Update tmux_monitor.sh

--- tmux_monitor.sh
+++ tmux_monitor.sh
@@ -7,15 +7,11 @@ WORK_DIR="rl-swarm"
 VENV_PATH=".venv/bin/activate"
 RUN_SCRIPT="./run_rl_swarm.sh"
 if ! /usr/bin/tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
-    cd /home/ubuntu
+    cd /home/ubuntu || exit 1
     sleep 1
-    /usr/bin/tmux new-session -d -s "$SESSION_NAME"
+    /usr/bin/tmux new-session -d -s "$SESSION_NAME" /bin/bash
     sleep 1
-    /usr/bin/tmux send-keys -t "$SESSION_NAME" "cd $WORK_DIR" Enter
-    sleep 1
-    /usr/bin/tmux send-keys -t "$SESSION_NAME" "source $VENV_PATH" Enter
-    sleep 1
-    /usr/bin/tmux send-keys -t "$SESSION_NAME" "$RUN_SCRIPT" Enter
+    /usr/bin/tmux send-keys -t "$SESSION_NAME" "cd $WORK_DIR && . $VENV_PATH && $RUN_SCRIPT" Enter
 fi
 SCRIPT
 
